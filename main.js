cf767280e9b47862baa5b03cb460d41efcd88cd4
Devapritam Sharma <dsarma1980@gmail.com>
2021-09-20 17:28:47 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,27 @@
 function preload() {}
 
 function setup() {
-    canvas = createCanvas(300, 300);
-    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(500, 450);
+    video.parent('videoDiv'); 
+
+    canvas = createCanvas(550, 550);
+    canvas.position(620, 150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    window.alert('Posenet Model has been initialized');
+}
+
+function draw() {
+    background('#969A97');
+}
+
+function gotPoses(results) {
+    if(results.length > 0) {
+        console.log(results);
+    }
 }
